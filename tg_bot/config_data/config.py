880d37616a3cad3613f2b3487fe629b62148b1fd
lubov <novahuaweidns@gmail.com>
2024-09-04 18:15:11 +0300

    i am begining again

--- tg_bot/config_data/config.py
+++ tg_bot/config_data/config.py
@@ -1,7 +1,6 @@
 import os
-from dotenv import find_dotenv
+from dotenv import find_dotenv, load_dotenv
 
-from flask.cli import load_dotenv
 
 if not find_dotenv():
     exit('Переменные окружения не найдены, т.к. отсутсвует файл .env')
@@ -11,6 +10,10 @@ else:
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 RAPID_API_KEY = os.getenv('RAPID_API_KEY')
 DEFAULT_COMMANDS = (
-    ('start', 'Запустить бота'),
-    ('help', 'Вывести справку')
+    ('start', "Запустить бота"),
+    ('help', "Вывести справку"),
+    ('lowprice', 'Поиск бюджетных отелей'),
+    ('highprice', 'Поиск лучших отелей'),
+    ('bestdeal', 'Настройка поиска'),
+    ('history', 'История поиска')
 )
--- tg_bot/loader.py
+++ tg_bot/loader.py
@@ -3,4 +3,4 @@ from telebot.storage import StateMemoryStorage
 from config_data import config
 
 storage = StateMemoryStorage()
-bot = TeleBot(Token=config.BOT_TOKEN, state_storage=storage)
+bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
